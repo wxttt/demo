6ba6e3286c48db4660be0d528be0472e02c63f08
wxttt <wangxiaottt@gmail.com>
2014-06-26 16:53:05 +0800

    quint demo

---
+++ playqunit/test.js
@@ -0,0 +1,15 @@
+function format(string, values) {
+    for (var key in values) {
+        string = string.replace(new RegExp("\{" + key + "}", "g"), values[key]);
+    }
+    return string;
+}
+
+test("basics", function() {
+    var values = {
+        name: "World"
+    };
+    equal( format("Hello, {name}", values), "Hello, World", "单个匹配" );
+    equal( format("Hello, {name}, how is {name} today?", values),
+        "Hello, World, how is World today?", "多个匹配" );
+});
---
+++ ranList/ranList.js
@@ -0,0 +1,7 @@
+/**
+ * Created with PyCharm.
+ * User: wangxiaottt
+ * Date: 14-4-1
+ * Time: 下午2:04
+ * To change this template use File | Settings | File Templates.
+ */
